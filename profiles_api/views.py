316ee67196cd6f762122738471a7426ce8176305
Aviv <avivkaplan00@gmail.com>
2023-02-10 10:21:45 +0200

    Added an APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,7 +10,11 @@ class HelloApiView(APIView):
         """Returns a list of APIView features"""
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
-            'Is similar to a traditional Django view'
-            'Gives you the most control over your application logic'
+            'Is similar to a traditional Django view',
+            'Gives you the most control over your application logic',
             'Is mapped manually to URLs'
         ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+
