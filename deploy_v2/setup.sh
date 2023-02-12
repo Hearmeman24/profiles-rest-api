55996bfd79c978b3fc13ea9bba4064760a546b90
Aviv <avivkaplan00@gmail.com>
2023-02-12 18:18:54 +0200

    Ready for deployment

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -15,13 +15,13 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 #mkdir -p $PROJECT_BASE_PATH
 #git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 #
-## Create virtual environment
-#mkdir -p $PROJECT_BASE_PATH/env
-#python3 -m venv $PROJECT_BASE_PATH/env
-#
-## Install python packages
-#$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
-#$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
+# Create virtual environment
+mkdir -p $PROJECT_BASE_PATH/env
+python3 -m venv $PROJECT_BASE_PATH/env
+
+# Install python packages
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
 # Run migrations and collectstatic
 cd $PROJECT_BASE_PATH
