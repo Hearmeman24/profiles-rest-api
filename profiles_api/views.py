1daba61ce14ffd3e819f31b7bc89bd9a12ce04c6
Aviv <avivkaplan00@gmail.com>
2023-02-12 17:25:00 +0200

    Added UserProfileFeedViewSet and permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,8 +5,10 @@ from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 from profiles_api import serializers, models
 from profiles_api import permissions
+
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -103,12 +105,20 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication token"""
-    def get(self, request, format=None):
-        """Returns a list of APIView features"""
-        hello_message = "This is a login view"
-
-        return Response(hello_message)
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
 
